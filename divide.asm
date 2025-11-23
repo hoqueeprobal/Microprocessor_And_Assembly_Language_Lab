519233e19205822a0df480647390545d2c2bd6c2
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2025-11-23 21:24:04 +0600

    Divide two ASCII digits and print result

---
+++ divide.asm
@@ -0,0 +1,31 @@
+.MODEL SMALL
+.STACK 100H  
+
+.DATA
+A DB 38H
+B DB 34H
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV AL, A
+    SUB AL, 30H  
+    
+    MOV BL, B
+    SUB BL, 30H
+
+    MOV AH, 0
+    DIV BL
+
+    ADD AL, 30H  
+    
+    MOV DL, AL
+    MOV AH, 2
+    INT 21H
+
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
