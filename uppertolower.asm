e20d8b6d87f124d9d53651f5bc04f75771e38d07
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2025-11-23 11:01:04 +0600

    Uppercase to Lowercase Conversion

---
+++ uppertolower.asm
@@ -0,0 +1,28 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV AH, 1
+    INT 21H
+
+    MOV BL, AL
+    ADD BL, 20H
+
+    MOV DL, AL
+    MOV AH, 2
+    INT 21H
+
+    MOV DL, BL
+    MOV AH, 2
+    INT 21H
+
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
