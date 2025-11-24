d25e48ccffcdc96db6ee5b9f6968e5860bc5a9f8
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2025-11-24 18:06:55 +0600

    A + (B + 1) Using Increment Operator

---
+++ increment.asm
@@ -0,0 +1,38 @@
+.MODEL SMALL
+.STACK 100H  
+
+.DATA
+A DB 34H      
+B DB 32H       
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+
+    MOV AL, A
+    SUB AL, 30H      
+    
+    
+    MOV BL, B
+    SUB BL, 30H      
+    INC BL           ; BL = B + 1
+
+    ;A + (B + 1)
+    ADD AL, BL       
+
+  
+    ADD AL, 30H
+
+
+    MOV DL, AL
+    MOV AH, 2
+    INT 21H
+
+
+
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
